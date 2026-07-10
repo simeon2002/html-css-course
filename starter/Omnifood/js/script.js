27a1f889bcd1a5bc6b933383ec783f6a2b7ddb7b
Simeon <61100437+simeon2002@users.noreply.github.com>
2026-07-10 20:30:31 +0300

    Feat: Add sticky navbar

--- starter/Omnifood/js/script.js
+++ starter/Omnifood/js/script.js
@@ -11,3 +11,24 @@ navLinks.forEach(navlink => {
     section.scrollIntoView({ behavior: "smooth" });
   });
 });
+
+// Adding sticky navbar
+const observer = new IntersectionObserver(cb, {
+  root: null,
+  rootMargin: "-96px",
+  threshold: 0,
+});
+
+observer.observe(document.querySelector(".hero-section"));
+
+function cb(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    console.log("test");
+    document.querySelector("body").classList.add("sticky");
+  } else if (entry.isIntersecting) {
+    console.log("removing ");
+    document.querySelector("body").classList.remove("sticky");
+  }
+}
