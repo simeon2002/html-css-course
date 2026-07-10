68babbd065b3f57e76bc07a5b606d569a54b2a93
Simeon <61100437+simeon2002@users.noreply.github.com>
2026-07-11 01:12:23 +0300

    Add mobile nav interactivity.
    Note: bug due to insufficient padding and too high sticky mobile navbar. Fixed in devtools with decreasing sticky navbar height + add more paddign to the sections themselves

--- starter/Omnifood/js/script.js
+++ starter/Omnifood/js/script.js
@@ -2,16 +2,6 @@
 const navLinks = Array.from(document.querySelectorAll(".main-nav-list a"));
 navLinks.push(...document.querySelectorAll(".hero-section a"));
 
-navLinks.forEach(navlink => {
-  navlink.addEventListener("click", e => {
-    e.preventDefault();
-    const sectionId = e.target.getAttribute("href");
-    const section = document.querySelector(sectionId);
-    console.log(section);
-    section.scrollIntoView({ behavior: "smooth" });
-  });
-});
-
 // Adding sticky navbar
 const observer = new IntersectionObserver(cb, {
   root: null,
@@ -23,7 +13,7 @@ observer.observe(document.querySelector(".hero-section"));
 
 function cb(entries, observer) {
   const [entry] = entries;
-
+  if (document.querySelector(".header").classList.contains("nav-open")) return;
   if (!entry.isIntersecting) {
     console.log("test");
     document.querySelector("body").classList.add("sticky");
@@ -32,3 +22,22 @@ function cb(entries, observer) {
     document.querySelector("body").classList.remove("sticky");
   }
 }
+
+// Adding mobile menu interactivity
+const btnMobileNav = document.querySelector(".btn-mobile-nav");
+btnMobileNav.addEventListener("click", e => {
+  document.querySelector(".header").classList.toggle("nav-open");
+});
+
+const navList = document.querySelector(".main-nav-list");
+navList.addEventListener("click", e => {
+  console.log(e.target);
+  if (e.target.tagName === "A") {
+    e.preventDefault();
+    document.querySelector(".header").classList.remove("nav-open");
+    const href = e.target.getAttribute("href");
+    const section = document.querySelector(href);
+    console.log(section);
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+});
